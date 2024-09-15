31d3424d499e985909556a3d8d96563a972b3e9f
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-09-15 13:20:27 -0700

    WIP added image predictor

--- src/napari_segment_anything_2/_tests/test_widget.py
+++ src/napari_segment_anything_2/_tests/test_widget.py
@@ -10,6 +10,66 @@ from skimage.data import astronaut, camera
 from napari_segment_anything_2 import SAM2Widget
 
 
+@pytest.fixture
+def sample_image() -> ArrayLike:
+    return astronaut()
+
+
+@pytest.mark.parametrize("im_dtype", [np.uint8, np.float32])
+def test_click(
+    make_napari_viewer: Callable[[], napari.Viewer],
+    sample_image: ArrayLike,
+    im_dtype: np.dtype,
+) -> None:
+    viewer = make_napari_viewer()
+    # viewer = napari.Viewer()
+    sample_image = sample_image.astype(im_dtype)
+
+    widget = SAM2Widget(viewer, model_type="sam2_hiera_b+")
+
+    viewer.window.add_dock_widget(widget)
+    viewer.add_image(sample_image)
+
+    assert widget._predictor is not None
+    assert widget._im_layer_widget.value is not None
+    assert not widget._confirm_mask_btn.enabled
+    assert not widget._cancel_annot_btn.enabled
+
+    # first interaction
+    widget._pts_layer.data = [[42, 233]]  # point on hair
+
+    assert np.any(widget._mask_layer.data > 0)
+    assert np.all(widget._labels_layer.data == 0)
+    assert widget._confirm_mask_btn.enabled
+    assert widget._cancel_annot_btn.enabled
+
+    # confirm mask
+    widget._confirm_mask_btn.clicked()
+    assert np.all(widget._mask_layer.data == 0)
+    assert np.any(widget._labels_layer.data > 0)
+    assert not widget._confirm_mask_btn.enabled
+    assert not widget._cancel_annot_btn.enabled
+
+    # new interaction
+    widget._pts_layer.data = [[42, 233], [125, 225]]  # adding point to face
+    assert np.any(widget._mask_layer.data > 0)
+
+    # confirm mask
+    widget._confirm_mask_btn.clicked()
+    assert np.all(widget._mask_layer.data == 0)
+    assert len(np.unique(widget._labels_layer.data)) == 3
+
+    # test cancel interaction
+    widget._pts_layer.data = [[42, 233]]
+    assert np.any(widget._mask_layer.data > 0)
+
+    widget._cancel_annot_btn.clicked()
+    assert np.any(widget._mask_layer.data == 0)
+    assert len(np.unique(widget._labels_layer.data)) == 3  # still the same
+    assert not widget._confirm_mask_btn.enabled
+    assert not widget._cancel_annot_btn.enabled
+
+
 @pytest.mark.parametrize(
     "image,is_video",
     itertools.product(
@@ -31,7 +91,7 @@ def test_automatic_segmentation(
     data = np.stack([image] * 3) if is_video else image
     viewer.add_image(data)
 
-    widget._run_btn.clicked()
+    widget._auto_segm_btn.clicked()
 
     if request.config.getoption("--show-napari-viewer"):
         napari.run()
--- src/napari_segment_anything_2/_widget.py
+++ src/napari_segment_anything_2/_widget.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import Any, Generator, Optional, Tuple
 
 import napari
 import numpy as np
@@ -13,8 +13,11 @@ from magicgui.widgets import (
     PushButton,
     create_widget,
 )
-from napari.layers import Image
+from napari.layers import Image, Points, Shapes
+from napari.layers.shapes._shapes_constants import Mode
+from qtpy.QtCore import Qt
 from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+from sam2.sam2_image_predictor import SAM2ImagePredictor
 from skimage import color, measure, transform, util
 
 from napari_segment_anything_2.sam2 import (
@@ -35,6 +38,7 @@ initialize_config_module(
 
 class SAM2Widget(Container):
     _predictor: SAM2VideoPredictor
+    _image_predictor: SAM2ImagePredictor
 
     def __init__(
         self, viewer: napari.Viewer, model_type: str = "sam2_hiera_b+"
@@ -62,17 +66,65 @@ class SAM2Widget(Container):
         self._im_layer_widget.changed.connect(self._load_image)
         self.append(self._im_layer_widget)
 
-        self._run_btn = PushButton(text="Auto. Segm.")
-        self._run_btn.changed.connect(self._on_run)
-        self.append(self._run_btn)
+        self._confirm_mask_btn = PushButton(
+            text="Confirm Annot.",
+            enabled=False,
+            tooltip="Press C to confirm annotation.",
+        )
+        self._confirm_mask_btn.changed.connect(self._on_confirm_mask)
+        self.append(self._confirm_mask_btn)
+
+        self._cancel_annot_btn = PushButton(
+            text="Cancel Annot.",
+            enabled=False,
+            tooltip="Press X to cancel annotation.",
+        )
+        self._cancel_annot_btn.changed.connect(self._cancel_annot)
+        self.append(self._cancel_annot_btn)
+
+        self._auto_segm_btn = PushButton(text="Auto. Segm.")
+        self._auto_segm_btn.changed.connect(self._on_auto_segm)
+        self.append(self._auto_segm_btn)
 
         self._tracks_ckbox = CheckBox(text="Show tracks")
         self._tracks_ckbox.value = False
         self.append(self._tracks_ckbox)
 
-        self._image: Optional[np.ndarray] = None
+        self._labels_layer = self._viewer.add_labels(
+            data=np.zeros((1, 256, 256), dtype=int),
+            name="SAM labels",
+        )
+
+        self._mask_layer = self._viewer.add_labels(
+            data=np.zeros((1, 256, 256), dtype=int),
+            name="SAM mask",
+            colormap={1: "cyan", None: "transparent"},
+        )
+        self._mask_layer.contour = 2
+
+        self._pts_layer = self._viewer.add_points(name="SAM points")
+        self._pts_layer.current_face_color = "blue"
+        self._pts_layer.events.data.connect(self._on_interactive_run)
+        self._pts_layer.mouse_drag_callbacks.append(
+            self._mouse_button_modifier
+        )
+        self._boxes_layer = self._viewer.add_shapes(
+            name="SAM box",
+            face_color="transparent",
+            edge_color="green",
+            edge_width=2,
+        )
+        self._boxes_layer.mouse_drag_callbacks.append(self._on_shape_drag)
+
+        self._init_frame: Optional[np.ndarray] = None
+        self._video: Optional[np.ndarray] = None
+        self._logits: Optional[torch.TensorType] = None
+
         self._model_type_widget.changed.emit(model_type)
         self._im_layer_widget.changed.emit(self._im_layer_widget.value)
+
+        self._viewer.bind_key("C", self._on_confirm_mask)
+        self._viewer.bind_key("X", self._cancel_annot)
 
     def _load_model(self, model_type: str) -> None:
         self._predictor = build_sam2_video_predictor(
@@ -80,41 +132,96 @@ class SAM2Widget(Container):
         )
         self._predictor.fill_hole_area = 0
         self._mask_generator = SAM2AutomaticMaskGenerator(self._predictor)
+        self._image_predictor = SAM2ImagePredictor(self._predictor)
 
+    @wait_cursor()
     def _load_image(self, im_layer: Optional[Image]) -> None:
         is_valid = False
+        self._init_frame, self._video = None, None
         if im_layer is not None:
             image = im_layer.data
-            if im_layer.ndim != 3 or im_layer.ndim != 2:
+            if im_layer.ndim < 2 or im_layer.ndim > 3:
                 print(
                     "Image must have 2 (YX) or 3 (TYX) plus optional RGB channels. "
                     f"Got {image.ndim}"
                 )
             else:
                 is_valid = True
+                self._init_frame, self._video = self._load_video()
 
-        self._run_btn.enabled = is_valid
+        self._auto_segm_btn.enabled = is_valid
+
+    def _mouse_button_modifier(self, _: Points, event) -> None:
+        self._pts_layer.selected_data = []
+        if event.button == Qt.LeftButton:
+            self._pts_layer.current_face_color = "blue"
+        else:
+            self._pts_layer.current_face_color = "red"
+
+    def _on_interactive_run(self, _: Optional[Any] = None) -> None:
+        points = self._pts_layer.data
+        boxes = self._boxes_layer.data
+
+        if self._im_layer_widget.value is None or (
+            len(points) == 0 and len(boxes) == 0
+        ):
+            return
+
+        if len(boxes) > 0:
+            box = boxes[-1]
+            box = np.stack([box.min(axis=0), box.max(axis=0)], axis=0)
+            box = np.flip(box, -1).reshape(-1)[None, ...]
+        else:
+            box = None
+
+        if len(points) > 0:
+            points = np.flip(points, axis=-1).copy()
+            colors = self._pts_layer.face_color
+            blue = [0, 0, 1, 1]
+            labels = np.all(colors == blue, axis=1)
+        else:
+            points = None
+            labels = None
+
+        mask, _, self._logits = self._image_predictor.predict(
+            point_coords=points,
+            point_labels=labels,
+            box=box,
+            mask_input=self._logits,
+            multimask_output=False,
+        )
+        self._mask_layer.data = mask.astype(int)
+        self._confirm_mask_btn.enabled = True
+        self._cancel_annot_btn.enabled = True
+
+    def _on_shape_drag(self, _: Shapes, event) -> Generator:
+        if self._boxes_layer.mode != Mode.ADD_RECTANGLE:
+            return
+        # on mouse click
+        yield
+        # on move
+        while event.type == "mouse_move":
+            yield
+        # on mouse release
+        self._on_interactive_run()
 
     @wait_cursor()
-    def _on_run(self) -> None:
+    def _on_auto_segm(self) -> None:
         if self._im_layer_widget.value is None:
             return
 
-        init_frame, video = self._load_video()
-
-        video = torch.flip(video, (0,))
         video_masks = np.zeros(
-            (video.shape[0], video.shape[2], video.shape[3]), dtype=np.int32
+            (self._video.shape[0], self._video.shape[2], self._video.shape[3]),
+            dtype=np.int32,
         )
 
         with torch.inference_mode(), torch.autocast(
             "cuda", dtype=torch.bfloat16
         ):
-
-            state = self._predictor.init_state(video)
+            state = self._predictor.init_state(self._video)
             self._predictor.reset_state(state)
 
-            masks = self._mask_generator.generate(init_frame)
+            masks = self._mask_generator.generate(self._init_frame)
             for i, item in enumerate(masks):
                 _, _, masks = self._predictor.add_new_mask(
                     state,
@@ -143,9 +250,10 @@ class SAM2Widget(Container):
             order=0,
         )
 
-        self._viewer.add_labels(video_masks, name="Masks")
+        self._labels_layer.data = video_masks
 
         if self._tracks_ckbox.value:
+            # TODO: make available in interactive mode
             df = []
             for t, frame in enumerate(video_masks):
                 _df = pd.DataFrame(
@@ -193,6 +301,15 @@ class SAM2Widget(Container):
         image = util.img_as_float(image)
         init_frame = image[-1].astype(np.float32)
 
+        self._mask_layer.data = np.zeros(
+            (1, *init_frame.shape[-3:-1]), dtype=int
+        )
+        self._labels_layer.data = np.zeros(
+            (1, *init_frame.shape[-3:-1]), dtype=int
+        )
+
+        self._image_predictor.set_image(init_frame)
+
         s = self._predictor.image_size
         image = transform.resize(
             image, (image.shape[0], s, s), order=1, anti_aliasing=True
@@ -204,4 +321,25 @@ class SAM2Widget(Container):
         tensor -= img_mean
         tensor /= img_std
 
+        tensor = torch.flip(tensor, (0,))
+
         return init_frame, tensor
+
+    def _on_confirm_mask(self, _: Optional[Any] = None) -> None:
+        if self._video is None:
+            return
+
+        labels = self._labels_layer.data
+        mask = self._mask_layer.data
+        labels[np.nonzero(mask)] = labels.max() + 1
+        self._labels_layer.data = labels
+        self._cancel_annot()
+
+    def _cancel_annot(self, _: Optional[Any] = None) -> None:
+        # boxes must be reset first because of how of points data update signal
+        self._boxes_layer.data = []
+        self._pts_layer.data = []
+        self._mask_layer.data = np.zeros_like(self._mask_layer.data)
+
+        self._confirm_mask_btn.enabled = False
+        self._cancel_annot_btn.enabled = False
